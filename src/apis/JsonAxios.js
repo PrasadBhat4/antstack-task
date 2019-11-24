957ce2611da33d9cf4d086520a56f3bd6e689860
Prasad Bhat <prasadbhat4569@gmail.com>
2019-11-24 19:07:28 +0530

    commit

--- src/apis/JsonAxios.js
+++ src/apis/JsonAxios.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL:
-    "https://node-server-app.netlify.com/?_ga=2.185764329.1123577233.1574581967-312596825.1573497240"
+  baseURL: "https://order-management-system.netlify.com/"
 });
